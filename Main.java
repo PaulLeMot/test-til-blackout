a9f7bfb2fa44ff03eb13d8a634f1b5899d2f40e9
aidar <ajdarmif@gmail.com>
2025-11-04 00:15:20 +0300

    Починил Api1

--- Main.java
+++ Main.java
@@ -31,13 +31,13 @@ public class Main {
 
         // Создаём сканеры - начинаем с основных
         List<SecurityScanner> securityScanners = Arrays.asList(
-//                new API1_BOLAScanner(),
-//                new API2_BrokenAuthScanner(),
-//                new API3_BOScanner(),
+                new API1_BOLAScanner(),
+                new API2_BrokenAuthScanner(),
+                new API3_BOScanner(),
 //                 Остальные пока закомментируем для теста
-//                 new API4_URCScanner(),
-//                 new API5_BrokenFunctionLevelAuthScanner(),
-//                 new API6_BusinessFlowScanner(),
+                 new API4_URCScanner(),
+                 new API5_BrokenFunctionLevelAuthScanner(),
+                 new API6_BusinessFlowScanner(),
                  new API7_SSRFScanner(),
                  new API8_SecurityConfigScanner(),
                  new API9_InventoryScanner(),
