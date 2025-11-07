ba6418b6cf12db6f6ce82b6f4b81e0186ac184b9
aidar <ajdarmif@gmail.com>
2025-11-08 01:33:41 +0300

    Фаззер почти готов но есть 403

--- scanners/fuzzing/FuzzingApiClient.java
+++
@@ -1,9 +0,0 @@
-// scanners/fuzzing/FuzzingApiClient.java
-package scanners.fuzzing;
-
-import java.util.Map;
-
-public interface FuzzingApiClient {
-    HttpResponse sendRequest(String method, String url, Map<String, String> params,
-                             Map<String, String> headers, Map<String, String> bodyParams);
-}
